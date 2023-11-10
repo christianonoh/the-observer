8c53e0df2f21e6f7ca7cf56cab1f383ce16df01b
christianonoh <onohchibyk@gmail.com>
2023-11-10 12:08:41 +0100

    Update Banner component styles and layout

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,13 +3,13 @@ import styles from "@/styles"
 
 const Banner = () => {
   return (
-    <div className={`${styles.xPaddings} py-2 border-b`}>
-      <div className="max-w-7xl mx-auto flex md:flex-row flex-col justify-between md:items-center">
-        <div className="text-slate-800">
+    <div className={`${styles.xPaddings} py-5 border-b`}>
+      <div className="max-w-7xl mx-auto flex lg:flex-row flex-col justify-between lg:items-center">
+        <div className="text-slate-800 font-bold">
           <h1 className="text-5xl">
             The Observer's Blog
           </h1>
-          <h2 className="mt-5 md-mt-0">
+          <h2 className="mt-5 lg:mt-3">
           Where
           {' '}
           <span className="underline decoration-primary decoration-4">
@@ -18,7 +18,7 @@ const Banner = () => {
           </span>
           </h2>
         </div>
-        <p className="mt-5 md:mt-2 text-slate-500 max-w-sm">
+        <p className="mt-5 lg:mt-0 text-slate-500 max-w-sm">
           Discover, Learn, Inspire. Your daily dose of diverse insights—from tech to travel, wellness to cuisine.
         </p>
       </div>
