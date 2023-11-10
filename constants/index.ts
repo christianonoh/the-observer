23a67e9f496285d98ad916fc1abea437f0b70b14
christianonoh <onohchibyk@gmail.com>
2023-11-10 02:37:50 +0100

    Add navigation items to constants

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    title: 'Home',
+    path: '/',
+  },
+  {
+    title: 'About',
+    path: '/about',
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+  },
+];
