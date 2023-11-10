45f47e63ea1a598f2004e3a311951053e4c6fd22
christianonoh <onohchibyk@gmail.com>
2023-11-10 11:52:41 +0100

    Add banner component to layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Work_Sans } from 'next/font/google'
 import '/styles/globals.css'
 import Header from '@/components/Header'
+import Banner from '@/components/Banner'
 
 const work = Work_Sans({ subsets: ['latin'] })
 
@@ -34,6 +35,7 @@ export default function RootLayout({
       </head>
       <body className={work.className}>
         <Header />
+        <Banner />
         <main>
           {children}
         </main>
