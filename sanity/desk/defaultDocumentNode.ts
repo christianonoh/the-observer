0a1d06b5b55fe2c5469ce96678cc31272440b3fa
Christian Onoh <chibyk5000@gmail.com>
2023-12-13 09:58:40 +0100

    Add defaultDocumentNode file for Sanity desk

---
+++ sanity/desk/defaultDocumentNode.ts
@@ -0,0 +1,23 @@
+// ./nextjs-app/sanity/desk/defaultDocumentNode.ts
+
+import {DefaultDocumentNodeResolver} from 'sanity/desk'
+import {Iframe} from 'sanity-plugin-iframe-pane'
+
+const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
+  switch (schemaType) {
+    case `post`:
+      return S.document().views([
+        S.view.form(),
+        S.view
+          .component(Iframe)
+          .options({
+            url: `/api/preview`,
+          })
+          .title('Preview'),
+      ])
+    default:
+      return S.document().views([S.view.form()])
+  }
+}
+
+export default defaultDocumentNode
