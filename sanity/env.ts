1f60b6d9442862fbb6266c3bb8f3a5dddbdd2e18
Christian Onoh <chibyk5000@gmail.com>
2023-12-22 23:06:32 +0100

    Update sanity/env.ts file

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,22 +1,30 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-11-08'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-11-08";
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+);
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+);
 
-export const useCdn = false
+export const useCdn = false;
+
+// This is the document id used for the preview secret that's stored in your dataset.
+// The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
+export const previewSecretId = process.env.NEXT_PUBLIC_SANITY_PREVIEW_SECRET;
+// "3b2b2e2d-5b0a-4b9e.8b9a-9e9b9c9d9e9f";
+
+// See the app/api/revalidate/route.ts for how this is used
+export const revalidateSecret = process.env.SANITY_REVALIDATE_SECRET;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
 
-  return v
+  return v;
 }
