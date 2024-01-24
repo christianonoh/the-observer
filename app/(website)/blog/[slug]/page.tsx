aea31b34bec18d267135846d7e67411803b1ffa0
Christian Onoh <chibyk5000@gmail.com>
2024-01-24 09:56:34 +0100

    Update dependencies and handle not found posts

--- app/(website)/blog/[slug]/page.tsx
+++ app/(website)/blog/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { sanityFetch, token } from "@/sanity/lib/fetch";
 import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
 import { SanityDocument } from "next-sanity";
 import { draftMode } from "next/headers";
+import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
 // Prepare Next.js to know which routes already exist
@@ -13,13 +14,18 @@ export async function generateStaticParams() {
   // Important, use the plain Sanity Client here
   const posts = await client.fetch(postPathsQuery);
 
-  return posts;
+  return posts.map((post: any) => ({
+    slug: post.params.slug,
+  }));
 }
 
 export default async function BlogPage({ params }: { params: any }) {
   const post = await sanityFetch<SanityDocument>({ query: postQuery, params });
   const isDraftMode = draftMode().isEnabled;
 
+  if (!post) {
+    notFound();
+  }
   if (isDraftMode && token) {
     return (
       <PreviewProvider token={token}>
